4dc8f656916d037e4a0ead56936c0e98efa07765
Awadhnaresh Nayak <nayakawadhnaresh11@gmail.com>
2025-12-25 18:27:30 +0530

    One commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,34 @@
-import React from "react";
-import { motion } from "framer-motion";
-import "../CSS/Home.css";
-import "../index.css";
+import React from 'react'
+import { motion } from 'framer-motion'
+import "../CSS/Home.css"
+import "../index.css"
 
 export default function Home() {
   const professions = [
-    "Git",
-    "MongoDB",
-    "MySQL",
-    "Socket Programming",
-    "MERN Stack Developer",
-  ];
+    'Git',
+    'MongoDB',
+    'MySQL',
+    'Socket Programming',
+    'MERN Stack Developer',
+  ]
 
   const quickLinks = [
-    { img: "/github.png", title: "GitHub", link: "https://github.com/AwadhnareshNayak" },
-    { img: "/linkedin.png", title: "LinkedIn", link: "https://www.linkedin.com/in/awadhnaresh-nayak-5a5977237/" },
-    { img: "/gmail.png", title: "Email", link: "mailto:awadhnaresh02@gmail.com" },
-    { img: "/whatsapp.png", title: "WhatsApp", link: "https://wa.me/+916260419871" },
-    { img: "/insta.png", title: "Instagram", link: "https://www.instagram.com/awadh_02" },
-    { img: "/facebook.png", title: "Facebook", link: "https://www.facebook.com/" },
-  ];
+    { img: '/github.png', title: 'GitHub', link: 'https://github.com/AwadhnareshNayak' },
+    { img: '/linkedin.png', title: 'LinkedIn', link: 'https://www.linkedin.com/in/awadhnaresh-nayak-5a5977237/' },
+    { img: '/gmail.png', title: 'Email', link: 'mailto:awadhnaresh02@gmail.com' },
+    { img: '/whatsapp.png', title: 'WhatsApp', link: 'https://wa.me/+916260419871' },
+    { img: '/insta.png', title: 'Instagram', link: 'https://www.instagram.com/awadh_02' },
+    { img: '/facebook.png', title: 'Facebook', link: 'https://www.facebook.com/share/1D1HYc7qC2/' },
+  ]
 
   return (
     <section className="home-section">
+
       <div className="home-top">
+
         {/* PHOTO */}
         <motion.div className="photo-container">
-          <div className="photo-ring" />
+          <div className="photo-ring"></div>
           <div className="photo-frame">
             <img
               src="/photo.jpg"
@@ -47,39 +49,37 @@ export default function Home() {
           </p>
 
           <div className="profession-tags">
-            {professions.map((role, i) => (
-              <div key={i} className="profession-tag">{role}</div>
+            {professions.map((item, i) => (
+              <div key={i} className="profession-tag">{item}</div>
             ))}
           </div>
 
           <div className="info-cards">
             <div className="info-card">
-              <strong>📍 Location</strong>
-              <p>Indore, Madhya Pradesh, India</p>
+              📍 <b>Location</b><br />Indore, MP, India
             </div>
             <div className="info-card">
-              <strong>💼 Expertise</strong>
-              <p>Developer, Security</p>
+              💼 <b>Expertise</b><br />Developer, Security
             </div>
             <div className="info-card">
-              <strong>📧 Contact</strong>
-              <p>awadhnaresh02@gmail.com</p>
+              📧 <b>Contact</b><br />awadhnaresh02@gmail.com
             </div>
           </div>
         </div>
       </div>
 
-      {/* SOCIAL LINKS */}
+      {/* SOCIAL */}
       <div className="quick-links">
         <h2 className="quick-links-title">Connect with me</h2>
         <div className="quick-links-list">
           {quickLinks.map((item, i) => (
-            <a key={i} href={item.link} target="_blank" rel="noreferrer">
+            <a key={i} href={item.link} target="_blank">
               <img src={item.img} alt={item.title} className="quick-link-img" />
             </a>
           ))}
         </div>
       </div>
+
     </section>
-  );
+  )
 }
